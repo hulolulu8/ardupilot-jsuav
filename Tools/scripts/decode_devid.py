tompoch2/ardupilot

Tools: scripts: add SCH16T to decode_devid.py

Tools/scripts/decode_devid.py
@@ -103,6 +103,7 @@ imu_types = {
     0x3A : "DEVTYPE_INS_ICM42670",
     0x3B : "DEVTYPE_INS_ICM45686",
     0x3C : "DEVTYPE_INS_SCHA63T",
+    0x3D : "DEVTYPE_INS_SCH16T",
 }
 
 baro_types = {

hulolulu8/ardupilot-jsuav

Co-authored-by: Jacob Dahl <dahl.jakejacob@gmail.com>
Co-authored-by: Peter Barker <pbarker@barker.dropbear.id.au>

Tools/scripts/decode_devid.py
@@ -110,6 +110,7 @@ imu_types = {
     0x3B : "DEVTYPE_INS_ICM45686",
     0x3C : "DEVTYPE_INS_SCHA63T",
     0x3D : "DEVTYPE_INS_IIM42653",
+    0x3E : "DEVTYPE_INS_SCH16T",
 }
 
 baro_types = {