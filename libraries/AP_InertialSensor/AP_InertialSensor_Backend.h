tompoch2/ardupilot

AP_InertialSensor: added driver for SCH16T

libraries/AP_InertialSensor/AP_InertialSensor_Backend.h
@@ -129,6 +129,7 @@ public:
         DEVTYPE_INS_ICM42670 = 0x3A,
         DEVTYPE_INS_ICM45686 = 0x3B,
         DEVTYPE_INS_SCHA63T  = 0x3C,
+        DEVTYPE_INS_SCH16T   = 0x3D,
     };
 
 protected:

hulolulu8/ardupilot-jsuav

Co-authored-by: Jacob Dahl <dahl.jakejacob@gmail.com>
Co-authored-by: Peter Barker <pbarker@barker.dropbear.id.au>

libraries/AP_InertialSensor/AP_InertialSensor_Backend.h
@@ -152,6 +152,7 @@ public:
         DEVTYPE_INS_ICM45686 = 0x3B,
         DEVTYPE_INS_SCHA63T  = 0x3C,
         DEVTYPE_INS_IIM42653 = 0x3D,
+        DEVTYPE_INS_SCH16T   = 0x3E,
     };
 
 protected: