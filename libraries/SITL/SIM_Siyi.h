peterbarker/ardupilot

SITL: add simulation for ZT30, Topotek and ViewPro gimbals

libraries/SITL/SIM_Siyi.h
@@ -1,0 +1,220 @@
+/*
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/*
+  Base class for Siyi camera simulators
+*/
+
+#pragma once
+
+#include "SIM_config.h"
+
+#if AP_SIM_SIYI_ENABLED
+
+#include "SIM_SerialDevice.h"
+
+#include <AP_Math/crc.h>
+#include <AP_Common/Location.h>
+#include <AP_InternalError/AP_InternalError.h>
+
+namespace SITL {
+
+class Siyi : public SerialDevice {
+public:
+
+    virtual void update(const class Aircraft &aircraft);
+
+protected:
+
+    static const uint8_t HEADER1 { 0x55 };
+    static const uint8_t HEADER2 { 0x66 };
+
+    template <typename T>
+    class PACKED PackedMessage {
+    public:
+        PackedMessage(T _msg) :
+            msg(_msg)
+        {
+            update_checksum();
+        }
+        uint8_t header1 { HEADER1 };
+        uint8_t header2 { HEADER2 };
+        uint8_t ctrl;
+        uint16_t datalen {sizeof(T)-1 };  // -1 to exinclude command ID
+        uint16_t seq;
+        T msg;
+        uint16_t checksum;
+
+        uint16_t calculate_checksum(uint16_t len) const WARN_IF_UNUSED {
+            return crc16_ccitt((uint8_t*)this, len, 0);
+        }
+        uint16_t calculate_checksum() const WARN_IF_UNUSED {
+            return calculate_checksum(sizeof(*this)-2);
+        }
+
+        void update_checksum() {
+            checksum = calculate_checksum();
+        }
+
+        bool response_requested() const { return (ctrl & 0x1) == 0x1; }
+    };
+
+    // message ids
+    enum class CommandID : uint8_t {
+        ACQUIRE_FIRMWARE_VERSION = 0x01,
+        HARDWARE_ID = 0x02,
+        // AUTO_FOCUS = 0x04,
+        // MANUAL_ZOOM_AND_AUTO_FOCUS = 0x05,
+        // MANUAL_FOCUS = 0x06,
+        GIMBAL_ROTATION = 0x07,
+        // CENTER = 0x08,
+        ACQUIRE_GIMBAL_CONFIG_INFO = 0x0A,
+        // FUNCTION_FEEDBACK_INFO = 0x0B,
+        PHOTO = 0x0C,
+        ACQUIRE_GIMBAL_ATTITUDE = 0x0D,
+        // ABSOLUTE_ZOOM = 0x0F,
+        SET_CAMERA_IMAGE_TYPE = 0x11,
+        GET_TEMP_FULL_IMAGE = 0x14,
+        READ_RANGEFINDER = 0x15,
+        EXTERNAL_ATTITUDE = 0x22,
+        SET_TIME = 0x30,
+        POSITION_DATA = 0x3e,
+    };
+
+    class PACKED BaseMessage {
+    public:
+        BaseMessage(CommandID _cmdid) :
+            cmdid{uint8_t(_cmdid)}
+        { }
+        uint8_t cmdid;
+    };
+
+    class PACKED FirmwareVersionRequest : public BaseMessage { };
+    virtual void handle_message(const PackedMessage<FirmwareVersionRequest> &request) = 0;
+
+    class PACKED HardwareIDRequest : public BaseMessage { };
+    virtual void handle_message(const PackedMessage<HardwareIDRequest> &request) = 0;
+
+    class PACKED GimbalAttitudeRequest : public BaseMessage { };
+    virtual void handle_message(const PackedMessage<GimbalAttitudeRequest> &request) = 0;
+
+    class PACKED ExternalAttitude : public BaseMessage {
+    public:
+        uint32_t time_boot_ms;
+        float roll, pitch, yaw;
+        float rollspeed, pitchspeed, yawspeed;
+    };
+    virtual void handle_message(const PackedMessage<ExternalAttitude> &request) {
+        AP_HAL::panic("Unhandled ExternalAttitude message");
+    }
+
+    class PACKED SetCameraImageType : public BaseMessage { };
+    virtual void handle_message(const PackedMessage<SetCameraImageType> &request) {
+        AP_HAL::panic("Unhandled SetCameraImageType message");
+    }
+
+    class PACKED Photo : public BaseMessage {
+    public:
+        uint8_t command;
+    };
+    virtual void handle_message(const PackedMessage<Photo> &request) {
+        AP_HAL::panic("Unhandled Photo message");
+    }
+
+    class PACKED AcquireGimbalConfigInfo : public BaseMessage {
+    public:
+    };
+    virtual void handle_message(const PackedMessage<AcquireGimbalConfigInfo> &request) {
+        AP_HAL::panic("Unhandled AcquireGimbalConfigInfo message");
+    }
+
+    class PACKED GetTempFullImageRequest : public BaseMessage {
+    public:
+        uint8_t mode;
+    };
+    virtual void handle_message(const PackedMessage<GetTempFullImageRequest> &request) { }
+
+    class PACKED ReadRangefinderRequest : public BaseMessage { };
+    virtual void handle_message(const PackedMessage<ReadRangefinderRequest> &request) { }
+
+    class PACKED GimbalRotation : public BaseMessage {
+    public:
+        uint8_t yaw_rate_pct;
+        uint8_t pitch_rate_pct;
+    };
+    virtual void handle_message(const PackedMessage<GimbalRotation> &request) {
+        AP_HAL::panic("Unhandled GimbalRotation message");
+    }
+
+    class PACKED PositionData : public BaseMessage {
+    public:
+        uint32_t time_boot_ms;
+        int32_t lat, lon;
+        int32_t alt_msl, alt_ellipsoid;
+        int32_t velocity_ned_int32[3];
+    };
+    virtual void handle_message(const PackedMessage<PositionData> &request) {
+        AP_HAL::panic("Unhandled PositionData message");
+    }
+
+    class PACKED SetTime : public BaseMessage {
+    public:
+        uint64_t epoch_us;
+    };
+    virtual void handle_message(const PackedMessage<SetTime> &request) {
+        AP_HAL::panic("Unhandled SetTime message");
+    }
+
+    /*
+     *  Input Handling
+     */
+    void update_input();
+    void handle_received_message();
+    uint32_t expected_message_length(CommandID id) const;
+
+    union u {
+        u() {}
+        uint8_t buffer[256]; // from-autopilot
+        PackedMessage<BaseMessage> packed_empty;
+        PackedMessage<FirmwareVersionRequest> packed_firmwareversionrequest;
+        PackedMessage<HardwareIDRequest> packed_hardwareidrequest;
+        PackedMessage<GimbalAttitudeRequest> packed_gimbalattituderequest;
+        PackedMessage<ExternalAttitude> packed_externalattitude;
+        PackedMessage<SetCameraImageType> packed_setcameraimgetype;
+        PackedMessage<Photo> packed_photo;
+        PackedMessage<AcquireGimbalConfigInfo> packed_acquiregimbalconfiginfo;
+        PackedMessage<GetTempFullImageRequest> packed_gettempfullimage;
+        PackedMessage<ReadRangefinderRequest> packed_readrangefinder;
+        PackedMessage<GimbalRotation> packed_gimbalrotation;
+        PackedMessage<PositionData> packed_positiondata;
+        PackedMessage<SetTime> packed_settime;
+    } msg;
+    uint8_t buflen;
+
+    void move_preamble_in_buffer(uint8_t search_start_pos=0);
+
+    /*
+     *  Output Handling
+     */
+    void update_output(const Location &location);
+
+    uint16_t expected_seq;
+
+    static const bool strict_parsing = true;
+
+};  // end class Siyi
+
+};  // end namespace SITL
+
+#endif  // AP_SIM_SIYI_ENABLED

libraries/SITL/SIM_Topotek.h
@@ -1,0 +1,77 @@
+/*
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/*
+  Simulator for Topotek gimbal
+
+./Tools/autotest/sim_vehicle.py --gdb --debug -v ArduCopter -A --serial5=sim:topotek --speedup=1
+
+param set MNT1_TYPE 12        # topotek
+param set SERIAL5_PROTOCOL 8  # gimbal
+reboot
+
+*/
+
+#pragma once
+
+#include "SIM_config.h"
+
+#if AP_SIM_TOPOTEK_ENABLED
+
+#include "SIM_SerialDevice.h"
+#include "SIM_Gimbal.h"
+
+namespace SITL {
+
+class Topotek : public SerialDevice {
+public:
+
+    void update(const Aircraft &aircraft);
+
+private:
+
+    // the physical gimbal:
+    Gimbal gimbal;
+
+    // input accumulation buffer; also used as working buffer by handle_packet()
+    static constexpr uint8_t PACKETLEN_MAX = 36;
+    uint8_t _buf[PACKETLEN_MAX];
+    uint8_t _buflen;
+
+    uint32_t _last_attitude_ms;     // time of last attitude packet sent
+
+    // read and dispatch incoming packets from autopilot
+    void update_input();
+
+    // send gimbal attitude packet to the driver
+    void send_attitude();
+
+    // dispatch a complete packet beginning at _buf[0], data_len data bytes
+    void handle_packet(uint8_t data_len);
+
+    // build and send a response packet
+    void send_packet(char addr2, const char id[3], bool write, const uint8_t *data, uint8_t len);
+
+    // encode a uint16 as 4 uppercase ASCII hex chars
+    static void uint16_to_hex4(uint16_t val, uint8_t buf[4]);
+
+    // convert a nibble (0-15) to an uppercase ASCII hex character
+    static uint8_t hex2char(uint8_t nibble) {
+        return nibble < 10 ? ('0' + nibble) : ('A' + nibble - 10);
+    }
+};
+
+}  // namespace SITL
+
+#endif  // AP_SIM_TOPOTEK_ENABLED

libraries/SITL/SIM_Viewpro.h
@@ -1,0 +1,119 @@
+/*
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/*
+  Simulator for Viewpro gimbal
+
+./Tools/autotest/sim_vehicle.py --gdb --debug -v ArduCopter -A --serial5=sim:viewpro --speedup=1
+
+param set MNT1_TYPE 11        # viewpro
+param set SERIAL5_PROTOCOL 8  # gimbal
+reboot
+
+*/
+
+#pragma once
+
+#include "SIM_config.h"
+
+#if AP_SIM_VIEWPRO_ENABLED
+
+#include "SIM_SerialDevice.h"
+#include "SIM_Gimbal.h"
+
+namespace SITL {
+
+class Viewpro : public SerialDevice {
+public:
+
+    void update(const Aircraft &aircraft);
+
+private:
+
+    // the physical gimbal:
+    Gimbal gimbal;
+
+    // Viewpro header bytes
+    static constexpr uint8_t HEADER1 = 0x55;
+    static constexpr uint8_t HEADER2 = 0xAA;
+    static constexpr uint8_t HEADER3 = 0xDC;
+    static constexpr uint8_t PACKETLEN_MAX = 63;
+
+    // frame IDs (matching AP_Mount_Viewpro.h)
+    enum class FrameId : uint8_t {
+        HANDSHAKE   = 0x00,
+        U           = 0x01,
+        V           = 0x02,
+        HEARTBEAT   = 0x10,
+        A1          = 0x1A,
+        C1          = 0x1C,
+        E1          = 0x1E,
+        C2          = 0x2C,
+        E2          = 0x2E,
+        T1_F1_B1_D1 = 0x40,
+        M_AHRS      = 0xB1,
+    };
+
+    // U packet communication configuration commands
+    enum class CommConfigCmd : uint8_t {
+        QUERY_FIRMWARE_VER = 0xD0,
+        QUERY_MODEL        = 0xE4,
+    };
+
+    // packet buffer.
+    // after header bytes are discarded by the state machine:
+    //   _buf[0] = length+frame_counter byte
+    //   _buf[1] = frame_id
+    //   _buf[2+] = data bytes
+    //   _buf[last] = CRC
+    uint8_t _buf[PACKETLEN_MAX];
+    uint8_t _buflen;
+
+    // parser state
+    enum class ParseState : uint8_t {
+        HEADER1,
+        HEADER2,
+        HEADER3,
+        LENGTH,
+        FRAMEID,
+        DATA,
+        CHECKSUM,
+    } _parse_state;
+
+    uint8_t _data_len;              // data bytes expected in current packet
+    uint8_t _data_bytes_received;   // data bytes received so far
+
+    uint8_t _frame_counter;         // 2-bit counter for outgoing packets
+    uint32_t _last_attitude_ms;     // time of last T1_F1_B1_D1 packet sent
+
+    // read and parse incoming bytes from autopilot
+    void update_input();
+
+    // dispatch a complete, CRC-verified packet in _buf
+    void dispatch_packet();
+
+    // send T1_F1_B1_D1 attitude status packet
+    void send_t1_f1_b1_d1();
+
+    // send V response to a U communication config command
+    void send_v_response(CommConfigCmd cmd);
+
+    // build and write a complete packet; databuff[0] = frame_id
+    void send_packet(const uint8_t *databuff, uint8_t databuff_len);
+
+};
+
+}  // namespace SITL
+
+#endif  // AP_SIM_VIEWPRO_ENABLED

hulolulu8/ardupilot-jsuav

libraries/SITL/SIM_Siyi.h
@@ -1,0 +1,220 @@
+/*
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/*
+  Base class for Siyi camera simulators
+*/
+
+#pragma once
+
+#include "SIM_config.h"
+
+#if AP_SIM_SIYI_ENABLED
+
+#include "SIM_Mount.h"
+
+#include <AP_Math/crc.h>
+#include <AP_Common/Location.h>
+#include <AP_InternalError/AP_InternalError.h>
+
+namespace SITL {
+
+class Siyi : public Mount {
+public:
+
+    void update(const class Aircraft &aircraft) override;
+
+protected:
+
+    static const uint8_t HEADER1 { 0x55 };
+    static const uint8_t HEADER2 { 0x66 };
+
+    template <typename T>
+    class PACKED PackedMessage {
+    public:
+        PackedMessage(T _msg) :
+            msg(_msg)
+        {
+            update_checksum();
+        }
+        uint8_t header1 { HEADER1 };
+        uint8_t header2 { HEADER2 };
+        uint8_t ctrl;
+        uint16_t datalen {sizeof(T)-1 };  // -1 to exinclude command ID
+        uint16_t seq;
+        T msg;
+        uint16_t checksum;
+
+        uint16_t calculate_checksum(uint16_t len) const WARN_IF_UNUSED {
+            return crc16_ccitt((uint8_t*)this, len, 0);
+        }
+        uint16_t calculate_checksum() const WARN_IF_UNUSED {
+            return calculate_checksum(sizeof(*this)-2);
+        }
+
+        void update_checksum() {
+            checksum = calculate_checksum();
+        }
+
+        bool response_requested() const { return (ctrl & 0x1) == 0x1; }
+    };
+
+    // message ids
+    enum class CommandID : uint8_t {
+        ACQUIRE_FIRMWARE_VERSION = 0x01,
+        HARDWARE_ID = 0x02,
+        // AUTO_FOCUS = 0x04,
+        // MANUAL_ZOOM_AND_AUTO_FOCUS = 0x05,
+        // MANUAL_FOCUS = 0x06,
+        GIMBAL_ROTATION = 0x07,
+        // CENTER = 0x08,
+        ACQUIRE_GIMBAL_CONFIG_INFO = 0x0A,
+        // FUNCTION_FEEDBACK_INFO = 0x0B,
+        PHOTO = 0x0C,
+        ACQUIRE_GIMBAL_ATTITUDE = 0x0D,
+        // ABSOLUTE_ZOOM = 0x0F,
+        SET_CAMERA_IMAGE_TYPE = 0x11,
+        GET_TEMP_FULL_IMAGE = 0x14,
+        READ_RANGEFINDER = 0x15,
+        EXTERNAL_ATTITUDE = 0x22,
+        SET_TIME = 0x30,
+        POSITION_DATA = 0x3e,
+    };
+
+    class PACKED BaseMessage {
+    public:
+        BaseMessage(CommandID _cmdid) :
+            cmdid{uint8_t(_cmdid)}
+        { }
+        uint8_t cmdid;
+    };
+
+    class PACKED FirmwareVersionRequest : public BaseMessage { };
+    virtual void handle_message(const PackedMessage<FirmwareVersionRequest> &request) = 0;
+
+    class PACKED HardwareIDRequest : public BaseMessage { };
+    virtual void handle_message(const PackedMessage<HardwareIDRequest> &request) = 0;
+
+    class PACKED GimbalAttitudeRequest : public BaseMessage { };
+    virtual void handle_message(const PackedMessage<GimbalAttitudeRequest> &request) = 0;
+
+    class PACKED ExternalAttitude : public BaseMessage {
+    public:
+        uint32_t time_boot_ms;
+        float roll, pitch, yaw;
+        float rollspeed, pitchspeed, yawspeed;
+    };
+    virtual void handle_message(const PackedMessage<ExternalAttitude> &request) {
+        AP_HAL::panic("Unhandled ExternalAttitude message");
+    }
+
+    class PACKED SetCameraImageType : public BaseMessage { };
+    virtual void handle_message(const PackedMessage<SetCameraImageType> &request) {
+        AP_HAL::panic("Unhandled SetCameraImageType message");
+    }
+
+    class PACKED Photo : public BaseMessage {
+    public:
+        uint8_t command;
+    };
+    virtual void handle_message(const PackedMessage<Photo> &request) {
+        AP_HAL::panic("Unhandled Photo message");
+    }
+
+    class PACKED AcquireGimbalConfigInfo : public BaseMessage {
+    public:
+    };
+    virtual void handle_message(const PackedMessage<AcquireGimbalConfigInfo> &request) {
+        AP_HAL::panic("Unhandled AcquireGimbalConfigInfo message");
+    }
+
+    class PACKED GetTempFullImageRequest : public BaseMessage {
+    public:
+        uint8_t mode;
+    };
+    virtual void handle_message(const PackedMessage<GetTempFullImageRequest> &request) { }
+
+    class PACKED ReadRangefinderRequest : public BaseMessage { };
+    virtual void handle_message(const PackedMessage<ReadRangefinderRequest> &request) { }
+
+    class PACKED GimbalRotation : public BaseMessage {
+    public:
+        uint8_t yaw_rate_pct;
+        uint8_t pitch_rate_pct;
+    };
+    virtual void handle_message(const PackedMessage<GimbalRotation> &request) {
+        AP_HAL::panic("Unhandled GimbalRotation message");
+    }
+
+    class PACKED PositionData : public BaseMessage {
+    public:
+        uint32_t time_boot_ms;
+        int32_t lat, lon;
+        int32_t alt_msl, alt_ellipsoid;
+        int32_t velocity_ned_int32[3];
+    };
+    virtual void handle_message(const PackedMessage<PositionData> &request) {
+        AP_HAL::panic("Unhandled PositionData message");
+    }
+
+    class PACKED SetTime : public BaseMessage {
+    public:
+        uint64_t epoch_us;
+    };
+    virtual void handle_message(const PackedMessage<SetTime> &request) {
+        AP_HAL::panic("Unhandled SetTime message");
+    }
+
+    /*
+     *  Input Handling
+     */
+    void update_input();
+    void handle_received_message();
+    uint32_t expected_message_length(CommandID id) const;
+
+    union u {
+        u() {}
+        uint8_t buffer[256]; // from-autopilot
+        PackedMessage<BaseMessage> packed_empty;
+        PackedMessage<FirmwareVersionRequest> packed_firmwareversionrequest;
+        PackedMessage<HardwareIDRequest> packed_hardwareidrequest;
+        PackedMessage<GimbalAttitudeRequest> packed_gimbalattituderequest;
+        PackedMessage<ExternalAttitude> packed_externalattitude;
+        PackedMessage<SetCameraImageType> packed_setcameraimgetype;
+        PackedMessage<Photo> packed_photo;
+        PackedMessage<AcquireGimbalConfigInfo> packed_acquiregimbalconfiginfo;
+        PackedMessage<GetTempFullImageRequest> packed_gettempfullimage;
+        PackedMessage<ReadRangefinderRequest> packed_readrangefinder;
+        PackedMessage<GimbalRotation> packed_gimbalrotation;
+        PackedMessage<PositionData> packed_positiondata;
+        PackedMessage<SetTime> packed_settime;
+    } msg;
+    uint8_t buflen;
+
+    void move_preamble_in_buffer(uint8_t search_start_pos=0);
+
+    /*
+     *  Output Handling
+     */
+    void update_output(const Location &location);
+
+    uint16_t expected_seq;
+
+    static const bool strict_parsing = true;
+
+};  // end class Siyi
+
+};  // end namespace SITL
+
+#endif  // AP_SIM_SIYI_ENABLED

libraries/SITL/SIM_Topotek.h
@@ -1,0 +1,81 @@
+/*
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/*
+  Simulator for Topotek gimbal
+
+./Tools/autotest/sim_vehicle.py --gdb --debug -v ArduCopter -A --serial5=sim:topotek --speedup=1
+
+param set MNT1_TYPE 12        # topotek
+param set SERIAL5_PROTOCOL 8  # gimbal
+reboot
+
+*/
+
+#pragma once
+
+#include "SIM_config.h"
+
+#if AP_SIM_TOPOTEK_ENABLED
+
+#include "SIM_Mount.h"
+#include "SIM_Gimbal.h"
+
+namespace SITL {
+
+class Topotek : public Mount {
+public:
+
+    void update(const Aircraft &aircraft) override;
+
+private:
+
+    // the physical gimbal:
+    Gimbal gimbal;
+
+    // input accumulation buffer; also used as working buffer by handle_packet()
+    static constexpr uint8_t PACKETLEN_MAX = 36;
+    uint8_t _buf[PACKETLEN_MAX];
+    uint8_t _buflen;
+
+    uint32_t _last_attitude_ms;     // time of last attitude packet sent
+
+    // last commanded angles from GIP/GIY packets (wire centidegrees, same sign as sent by driver)
+    int16_t _commanded_pitch_cd = 0;
+    int16_t _commanded_yaw_cd   = 0;
+
+    // read and dispatch incoming packets from autopilot
+    void update_input();
+
+    // send gimbal attitude packet to the driver
+    void send_attitude();
+
+    // dispatch a complete packet beginning at _buf[0], data_len data bytes
+    void handle_packet(uint8_t data_len);
+
+    // build and send a response packet
+    void send_packet(char addr2, const char id[3], bool write, const uint8_t *data, uint8_t len);
+
+    // encode a uint16 as 4 uppercase ASCII hex chars
+    static void uint16_to_hex4(uint16_t val, uint8_t buf[4]);
+
+    // convert a nibble (0-15) to an uppercase ASCII hex character
+    static uint8_t hex2char(uint8_t nibble) {
+        return nibble < 10 ? ('0' + nibble) : ('A' + nibble - 10);
+    }
+};
+
+}  // namespace SITL
+
+#endif  // AP_SIM_TOPOTEK_ENABLED

libraries/SITL/SIM_Viewpro.h
@@ -1,0 +1,124 @@
+/*
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation, either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+/*
+  Simulator for Viewpro gimbal
+
+./Tools/autotest/sim_vehicle.py --gdb --debug -v ArduCopter -A --serial5=sim:viewpro --speedup=1
+
+param set MNT1_TYPE 11        # viewpro
+param set SERIAL5_PROTOCOL 8  # gimbal
+reboot
+
+*/
+
+#pragma once
+
+#include "SIM_config.h"
+
+#if AP_SIM_VIEWPRO_ENABLED
+
+#include "SIM_Mount.h"
+#include "SIM_Gimbal.h"
+
+namespace SITL {
+
+class Viewpro : public Mount {
+public:
+
+    void update(const Aircraft &aircraft) override;
+
+private:
+
+    // the physical gimbal:
+    Gimbal gimbal;
+
+    // Viewpro header bytes
+    static constexpr uint8_t HEADER1 = 0x55;
+    static constexpr uint8_t HEADER2 = 0xAA;
+    static constexpr uint8_t HEADER3 = 0xDC;
+    static constexpr uint8_t PACKETLEN_MAX = 63;
+
+    // frame IDs (matching AP_Mount_Viewpro.h)
+    enum class FrameId : uint8_t {
+        HANDSHAKE   = 0x00,
+        U           = 0x01,
+        V           = 0x02,
+        HEARTBEAT   = 0x10,
+        A1          = 0x1A,
+        C1          = 0x1C,
+        E1          = 0x1E,
+        C2          = 0x2C,
+        E2          = 0x2E,
+        T1_F1_B1_D1 = 0x40,
+        M_AHRS      = 0xB1,
+    };
+
+    // U packet communication configuration commands
+    enum class CommConfigCmd : uint8_t {
+        QUERY_FIRMWARE_VER = 0xD0,
+        QUERY_MODEL        = 0xE4,
+    };
+
+    // packet buffer.
+    // after header bytes are discarded by the state machine:
+    //   _buf[0] = length+frame_counter byte
+    //   _buf[1] = frame_id
+    //   _buf[2+] = data bytes
+    //   _buf[last] = CRC
+    uint8_t _buf[PACKETLEN_MAX];
+    uint8_t _buflen;
+
+    // parser state
+    enum class ParseState : uint8_t {
+        HEADER1,
+        HEADER2,
+        HEADER3,
+        LENGTH,
+        FRAMEID,
+        DATA,
+        CHECKSUM,
+    } _parse_state;
+
+    uint8_t _data_len;              // data bytes expected in current packet
+    uint8_t _data_bytes_received;   // data bytes received so far
+
+    uint8_t _frame_counter;         // 2-bit counter for outgoing packets
+    uint32_t _last_attitude_ms;     // time of last T1_F1_B1_D1 packet sent
+
+    // last A1 angle target received from driver, stored as raw int16 wire values
+    // (same encoding used in T1_F1_B1_D1 replies so the driver round-trips exactly)
+    int16_t _target_pitch_raw = 0;
+    int16_t _target_yaw_raw   = 0;
+
+    // read and parse incoming bytes from autopilot
+    void update_input();
+
+    // dispatch a complete, CRC-verified packet in _buf
+    void dispatch_packet();
+
+    // send T1_F1_B1_D1 attitude status packet
+    void send_t1_f1_b1_d1();
+
+    // send V response to a U communication config command
+    void send_v_response(CommConfigCmd cmd);
+
+    // build and write a complete packet; databuff[0] = frame_id
+    void send_packet(const uint8_t *databuff, uint8_t databuff_len);
+
+};
+
+}  // namespace SITL
+
+#endif  // AP_SIM_VIEWPRO_ENABLED